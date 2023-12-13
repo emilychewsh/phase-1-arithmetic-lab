0e90e7fe901f6e19c40ec22b71a5a72141d7c9e5
Emily Chew <emilychewsh@gmail.com>
2023-12-13 15:15:04 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 2) + 1
+console.log(random)
+
+let num3 = 4 
+let num4 = 6
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(1, 3, 5, 15, 20)
+console.log(max)
